f33fc09c959b51369ef6d8d0c9a93c209fdd4af6
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-01 10:14:28 -0500

    :tada: Begin

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::env;
+
+fn get_message() -> String {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() >= 1 {
+        args.join(" ")
+    } else {
+        "y".to_string()
+    }
+}
+
+fn main() {
+    let print_until_dead = get_message();
+
+    loop {
+        println!("{}", print_until_dead);
+    }
+}
